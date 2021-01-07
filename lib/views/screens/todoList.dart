9edef2e527b7b40fc7b156f1892741f4afbd96a4
Hasan Genc <hasan.genc@trendyol.com>
2021-01-07 22:49:50 +0300

    add todolist

--- lib/views/screens/todoList.dart
+++ lib/views/screens/todoList.dart
@@ -1,10 +1,57 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:personal_toolbox/models/todoList.dart';
+import 'package:personal_toolbox/services/todoListService.dart';
 import 'package:personal_toolbox/views/widgets/languageSwitch.dart';
 import 'package:personal_toolbox/views/widgets/todoItem.dart';
 
-class TodoList extends StatelessWidget {
+class TodoList extends StatefulWidget {
+  final service = TodoListService();
+  @override
+  TodoListState createState() => TodoListState();
+}
+
+class TodoListState extends State<TodoList> {
+  List<TodoItemModel> items;
+  void addItem(TodoItemModel value) {
+    widget.service.addItem(value).then((value) => {this.getItems()});
+  }
+
+  void updateItem(TodoItemModel value) {
+    widget.service.updateItem(value).then((value) => {this.getItems()});
+  }
+
+  void getItems() async {
+    List<TodoItemModel> _items = await widget.service.getItems();
+
+    if (_items.isNotEmpty) {
+      setState(() {
+        items = _items;
+      });
+    }
+  }
+
+  void addEmptyItem() {
+    if ((items != null &&
+        items.isNotEmpty &&
+        (items.first.id == null ||
+            items.first.title == null ||
+            items.first.title == ''))) {
+      return;
+    }
+    setState(() {
+      List rest = items != null ? items : [];
+      items = [TodoItemModel(null, '', '', false, false), ...rest];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getItems();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,22 +67,24 @@ class TodoList extends StatelessWidget {
                   height: MediaQuery.of(context).size.height * 0.60,
                   child: Column(children: <Widget>[
                     Expanded(
-                        child: ListView(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      physics: const AlwaysScrollableScrollPhysics(),
-                      children: <Widget>[
-                        TodoItem(),
-                        Text(
-                          'Hasan',
-                          style: TextStyle(fontSize: 30),
-                        ),
-                        Text(
-                          'Hasan',
-                          style: TextStyle(fontSize: 30),
-                        )
-                      ],
-                    )),
+                      child: items != null && items.length > 0
+                          ? ListView.builder(
+                              scrollDirection: Axis.vertical,
+                              shrinkWrap: true,
+                              physics: const AlwaysScrollableScrollPhysics(),
+                              itemCount: items.length,
+                              itemBuilder: (context, i) {
+                                return TodoItem(
+                                    key: Key(items[i].id.toString()),
+                                    value: items[i].title,
+                                    addItemCallback: items[i].id != null
+                                        ? updateItem
+                                        : addItem,
+                                    id: items[i].id);
+                              },
+                            )
+                          : Text('empty'),
+                    ),
                   ])),
               Ink(
                 decoration: const ShapeDecoration(
@@ -45,7 +94,7 @@ class TodoList extends StatelessWidget {
                 child: IconButton(
                   icon: Icon(Icons.add),
                   color: Colors.white,
-                  onPressed: () {},
+                  onPressed: addEmptyItem,
                 ),
               ),
             ]),
