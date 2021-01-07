80ffd469b8fab9293a189be266bc59f68a64eb1b
Hasan Genc <hasan.genc@trendyol.com>
2021-01-07 22:49:57 +0300

    add styles

---
+++ lib/views/styles/textField.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+final textFieldTextStyle =
+    TextStyle(fontSize: 18, height: 2.0, color: Colors.white);
+
+final textFieldHintStyle =
+    TextStyle(fontSize: 18, height: 2.0, color: Colors.grey);
+
+final textFieldBoxDecoration = BoxDecoration(
+    color: Colors.blueGrey, borderRadius: BorderRadius.all(Radius.circular(6)));
