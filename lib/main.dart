0fdfa38b97332c95720c5ed82fd72a844ea19049
Hasan Genc <hasan.genc@trendyol.com>
2021-01-15 20:50:39 +0300

    Change theme dynamically

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:personal_toolbox/views/screens/home.dart';
@@ -13,14 +14,27 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Personal Toolbox App',
-      theme:
-          ThemeData(brightness: Brightness.dark, primarySwatch: Colors.orange),
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      home: Home(),
+    return AdaptiveTheme(
+      light: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.red,
+        accentColor: Colors.amber,
+      ),
+      dark: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.red,
+        accentColor: Colors.amber,
+      ),
+      initial: AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        title: 'Personal Toolbox App',
+        theme: theme,
+        darkTheme: darkTheme,
+        home: Home(),
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+      ),
     );
   }
 }
